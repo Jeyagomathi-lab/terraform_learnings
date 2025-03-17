4bc538ed1a677e21d03181eedecfe86dbb16958f
Jeyagomathi-lab <jeyaawslearn@gmail.com>
2025-03-17 12:34:35 +0530

    Create variables.tf

---
+++ create_s3_bucket_and_policy/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    description = "Region where bucket needs to create"
+    type = string
+}
+
+variable "bucket_name" {
+    description = "Name for bucket"
+    type = string
+}
